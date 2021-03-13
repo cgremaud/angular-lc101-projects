212abe193e2871389bc4a1090365901433cf8f14
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-12 23:19:59 -0600

    added fav links component.

--- lesson1/exercises/src/app/fav-links/fav-links.component.ts
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-fav-links',
+  selector: 'fav-links',
   templateUrl: './fav-links.component.html',
   styleUrls: ['./fav-links.component.css']
 })
 export class FavLinksComponent implements OnInit {
-
+  favLinks = [
+    {
+    url: "https://www.google.com",
+    name: "google"
+   },
+   {
+     url: "https://facebook.com",
+    name: "facebook"
+   }, 
+   {
+     url: "https://soundcloud.com/colyngremaud",
+    name: "soundcloud"
+  }
+  ]
   constructor() { }
 
   ngOnInit() {
