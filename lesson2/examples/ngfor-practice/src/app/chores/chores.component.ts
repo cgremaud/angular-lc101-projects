dbc2cda257b412d570f021b5bd786ffd445b68c5
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-13 18:01:21 -0600

    chores list now loops over an array of objects to fill each section uniquely.

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
+   chores = [
+      {title: "Yesterday's Chores", tasks: ['Empty dishwasher', 'bark dogs', 'spay nutes']},
+      {title: "Today's Chores", tasks: ['Wallow in filth', 'Elate the lord', 'Spray defensive chemicals']},
+      {title: "Tomorrow's Chores", tasks: ['Allay fears', 'boil sinners in oil', 'praise jeebus']}
+   ]
+   
+   
    todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
 
    constructor() { }
