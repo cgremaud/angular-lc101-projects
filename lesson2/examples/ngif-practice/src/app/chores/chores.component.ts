f396af66097cb09e63de117aa70789649984db8d
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-13 21:13:44 -0600

    finishing ngif practice

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Elaborate on that', 'Initiate a fight'];
+   chores = ['Elaborate on that', 'Initiate a fight', 'break knees', 'type letter on human skin', 'dribble urine on spaghetti', 'work harder'];
    finishedChores = ['sprained ankles', 'popped wheelies', 'ate shit', 'killed god'];
 
    targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
