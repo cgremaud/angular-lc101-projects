7e8567a93906f10fbc04de2c19f471c599d484ca
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-14 20:00:27 -0500

    finished most of the booster rockets

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -35,6 +35,10 @@ export class CandidatesComponent implements OnInit {
   }
 
   // BONUS: Code the changeMissionName function here:
-
+  
+  changeMissionName (name:string) {
+    this.missionName = name;
+    
+  }
 
 }
