cf96e6b4e7e0e3818d735ab859f407aa7bbab61e
C. Gremaud <colyn.gremaud@gmail.com>
2021-03-22 20:43:50 -0500

    added a bunch of stuff

--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -9,11 +9,18 @@ export class SkillSetComponent implements OnInit {
    listHeading: string = 'Some Coding Skills I Know';
    skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
    alternateColor: string = 'white';
+   underlineOn: string = "underline"
+   italicsOn: string = "italic"
    bulletType: string = 'A';
    changeColor: boolean = true;
+   addOtherList: boolean = true;
+   otherList: string[] = ['Boobs', 'cats', 'smiles']
+   listA: string[] = ['hallelujah', 'alabama', 'jesus', 'electric eels']
 
    constructor() { }
 
    ngOnInit() { }
 
 }
+
+
